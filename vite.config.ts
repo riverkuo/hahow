b673b90fcc61b3ee67ba34d1369e57dfb806bd24
River <river@jgallop.com>
2025-09-01 13:47:01 +0800

    try fix：部署，vite config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'github-pages' ? '/hahow/' : '/',
+  base: process.env.NODE_ENV !== 'development' ? 'hahow' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // 手動映射
